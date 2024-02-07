6c4c1e2371704c27fd24ee48cd9680c6b4c7a1be
zivkovicmarko95 <zivkovic.marko.ftn@gmail.com>
2024-02-07 13:28:44 +0100

    Init spring quart web client

---
+++ spring-quartz-web-client/src/main/java/com/example/springquartzwebclient/SpringQuartzWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springquartzwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuartzWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuartzWebClientApplication.class, args);
+	}
+
+}
---
+++ spring-quartz-web-client/src/test/java/com/example/springquartzwebclient/SpringQuartzWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springquartzwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuartzWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
