879f59ec5b9c6ba7225b21c3f4c165b729873faa
zivkovicmarko95 <zivkovic.marko.ftn@gmail.com>
2024-04-05 20:41:46 +0200

    Initialize spring-redis-open-api application

---
+++ spring-redis-open-api/src/main/java/com/example/springredisopenapi/SpringRedisOpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springredisopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisOpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisOpenApiApplication.class, args);
+	}
+
+}
---
+++ spring-redis-open-api/src/test/java/com/example/springredisopenapi/SpringRedisOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springredisopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisOpenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
