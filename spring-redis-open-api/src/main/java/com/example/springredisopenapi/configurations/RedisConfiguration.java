5287af071fe944942e4012fdb53ddb4021dc07f2
zivkovicmarko95 <zivkovic.marko.ftn@gmail.com>
2024-04-05 20:42:26 +0200

    Add redis configuration

---
+++ spring-redis-open-api/src/main/java/com/example/springredisopenapi/configurations/RedisConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.springredisopenapi.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfiguration {
+    
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(final RedisConnectionFactory connectionFactory) {
+        
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+
+}
